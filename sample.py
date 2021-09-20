1a12662bbfe3aff6f824a518402fcb7334035854
Călina Cenan <calina@kaolin.tech>
2021-09-20 09:40:16 +0300

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import os
+import time
+import json
+
+def get_job_details():
+    """Reads in metadata information about assets used by the algo"""
+    job = dict()
+    job['dids'] = json.loads(os.getenv('DIDS', None))
+    job['metadata'] = dict()
+    job['files'] = dict()
+    job['algo'] = dict()
+    job['secret'] = os.getenv('secret', None)
+    algo_did = os.getenv('TRANSFORMATION_DID', None)
+    if job['dids'] is not None:
+        for did in job['dids']:
+            # get the ddo from disk
+            filename = '/data/ddos/' + did
+            print(f'Reading json from {filename}')
+            with open(filename) as json_file:
+                ddo = json.load(json_file)
+                # search for metadata service
+                print(ddo)
+                for service in ddo['service']:
+                    if service['type'] == 'metadata':
+                        job['files'][did] = list()
+                        index = 0
+                        for file in service['attributes']['main']['files']:
+                            job['files'][did].append(
+                                '/data/inputs/' + did + '/' + str(index))
+                            index = index + 1
+    if algo_did is not None:
+        job['algo']['did'] = algo_did
+        job['algo']['ddo_path'] = '/data/ddos/' + algo_did
+    return job
+
+
+def line_counter(job_details):
+    """Executes the line counter based on inputs"""
+    print('Starting compute job with the following input information:')
+    print(json.dumps(job_details, sort_keys=True, indent=4))
+
+    """ Now, count the lines of the first file in first did """
+    first_did = job_details['dids'][0]
+    filename = job_details['files'][first_did][0]
+    non_blank_count = 0
+    with open(filename) as infp:
+        for line in infp:
+            if line.strip():
+                non_blank_count += 1
+    print ('number of non-blank lines found %d' % non_blank_count)
+    """ Print that number to output to generate algo output"""
+    f = open("/data/outputs/result", "w")
+    f.write(str(non_blank_count))
+    f.close()
+
+
+if __name__ == '__main__':
+    line_counter(get_job_details())
